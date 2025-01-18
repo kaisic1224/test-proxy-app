7780c3f41064ef842e53b557c2c2bfbb89540ce7
kaisic1224 <hiddenshadows1224@gmail.com>
2025-01-18 01:09:16 -0600

    Change component to suspense

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,16 @@
-'use client'
+import { Suspense } from "react";
 
 export default async function ContactPage() {
   const data = await fetch("http://swapi.dev/api/people/2");
   const json = await data.json();
   return (
-    <div className="">
-      <div className="">
-        <h1 className="text-2xl">Contact</h1>
-        <code>{JSON.stringify(json, null, 4)}</code>
-      </div>
-    </div>
+      <Suspense>
+        <div className="">
+          <div className="">
+            <h1 className="text-2xl">Contact</h1>
+            <code>{JSON.stringify(json, null, 4)}</code>
+          </div>
+        </div>
+    </Suspense>
   );
 }
